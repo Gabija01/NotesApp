1b17ffe9f93d4ddd551a73b08d78fb15fc0b898c
Gabija01 <gabija.rutkauskaite@knf.stud.vu.lt>
2024-11-12 19:31:43 +0200

    Created add note button

--- app/src/main/java/com/example/notesapp/AddNoteActivity.java
+++ app/src/main/java/com/example/notesapp/AddNoteActivity.java
@@ -1,24 +1,38 @@
 package com.example.notesapp;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class AddNoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_add_note);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button saveButton = findViewById(R.id.btnSaveNote);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = ((EditText) findViewById(R.id.etNoteTitle)).getText().toString();
+                String content = ((EditText) findViewById(R.id.etNoteContent)).getText().toString();
+
+                if (!title.isEmpty() && !content.isEmpty()) {
+                    SharedPreferences prefs = getSharedPreferences("NotesApp", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putString(title, content);
+                    editor.apply();
+
+                    finish();
+                } else {
+                    Toast.makeText(AddNoteActivity.this, "Pavadinimas ir turinys negali būti tušti", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
     }
 }
